62998f9d82d6d2aab0d344de44c71863f1377cd9
guiwitz <guiwitz@users.noreply.github.com>
2025-01-30 13:26:12 +0100

    widget cleanup

--- src/napari_imagegrains/__init__.py
+++ src/napari_imagegrains/__init__.py
@@ -6,13 +6,10 @@ except ImportError:
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from .imagegrains_widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from .imagegrains_widget import ImageGrainWidget
 
 __all__ = (
     "napari_get_reader",
     "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    "ImageGrainWidget",
 )
